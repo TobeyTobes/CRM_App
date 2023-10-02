4f7cb4da845149dd209878b357f9765d8958987f
Austin Tobey <ajtobey93@gmail.com>
2023-10-02 15:15:03 -0500

    Updated

--- webApp/views.py
+++ webApp/views.py
@@ -82,3 +82,15 @@ def delete_record(request, pk):
         return redirect('home')
 
 
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Record Updated!")
+            return redirect('home')
+        return render(request, 'update_record.html', {'form':form})
+    else:
+        messages.success(request, "You Need to Be Logged In to Do That!")
+        return redirect('home')
